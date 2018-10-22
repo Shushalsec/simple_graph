e12ee1e505630a3b9cd83b840f37c4d7d48e433a
Shushalsec <shtoneyan@gmail.com>
2018-10-22 16:57:58 +0200

    Function added for getting crypts from flowers

--- segment.py
+++ segment.py
@@ -1,112 +1,77 @@
-import cv2
+"""
+Scripts 2: Metadater
+
+This script allows the user to extract information about the crypts in the given images. It is assumed that the input
+is a directory with subdirectories each containing the cropped image region, an image with same magnification for the
+mask of annotation from QuPath script output.
+
+This script requires that opencv be installed in the Python environment you are running this script in.
+
+This file should be imported as a module and contains the following function:
+
+    * crypt_data_extracter - returns features of the crypt
+"""
 import os
 import matplotlib.pyplot as plt
+import cv2
 import numpy as np
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import cm
-from matplotlib import colors
 
 
-
-BASE_DIR = 'C:/Users/st18l084/Dropbox/'
-FILE = 'nemo0.jpg'
-WSI = '366b_2.jpg'
-flags = [i for i in dir(cv2) if i.startswith('COLOR_BGR')]
-flags
-nemo = cv2.imread(os.path.join(BASE_DIR, WSI))
-nemo = cv2.cvtColor(nemo, cv2.COLOR_BGRA2RGB)
-plt.imshow(nemo)
-
-r, g, b = cv2.split(nemo)
-fig = plt.figure()
-axis = fig.add_subplot(1, 1, 1, projection="3d")
-
-pixel_colors = nemo.reshape((np.shape(nemo)[0]*np.shape(nemo)[1], 3))
-norm = colors.Normalize(vmin=-1.,vmax=1.)
-norm.autoscale(pixel_colors)
-pixel_colors = norm(pixel_colors).tolist()
-axis.scatter(r.flatten(), g.flatten(), b.flatten(), facecolors=pixel_colors, marker=".")
-
-hsv_nemo = cv2.cvtColor(nemo, cv2.COLOR_RGB2HSV)
-h, s, v = cv2.split(hsv_nemo)
-fig = plt.figure()
-axis = fig.add_subplot(1, 1, 1, projection="3d")
-
-axis.scatter(h.flatten(), s.flatten(), v.flatten(), facecolors=pixel_colors, marker=".")
-axis.set_xlabel("Hue")
-axis.set_ylabel("Saturation")
-axis.set_zlabel("Value")
-plt.show()
-from matplotlib.colors import hsv_to_rgb
-center[:,0]
-x = center[:,0]
-x
-square = np.full((10, 10, 3), x, dtype=np.uint8) / 255.0
-plt.imshow(hsv_to_rgb(square))
-plt.show()
+def checkpoint(subdir):
+    files = os.listdir(subdir)
+    if '-cropped' in files and '-mask' in files:
+        print('All images required are present!')
+        return True
+    else:
+        print('Images missing or have been named incorrectly')
+        return False
 
 
+def one_crypt_extracter(subdirectory):
+    """
+
+    :param subdirectory: subfolder containing only cropped image, its mask and .txt file
+    :return: features of crypt - at the moment the fraction of pixels crypt-y (white) enough out of the pixels
+    WITHIN the annotated region of the cropped image
+    """
+    # check if the subdirectory contains the required files
+    checkpoint(subdirectory)
+    # read images into numpy arrays
+    for file in os.listdir(subdirectory):
+        if '-cropped' in file:  # get the cropped image from the folder
+            flower_file = os.path.join(subdirectory, file)  # get the path to the cropped image file
+            flower = plt.imread(flower_file)  # get the numpy array of the cropped image
+        elif '-mask' in file:  # get the annotation mask in the folder
+            flower_mask_file = os.path.join(subdirectory, file)  # get the path to the annotation mask image
+            annotation_mask = plt.imread(flower_mask_file)  # get the mask as a numpy array
+    bool_mask = annotation_mask.astype(bool)  # convert to boolean array from float array
+    flower_copy = flower.copy()  # make a copy as the original is read only
+    # set the values in the copied image as 0, i.e. black pixel if they are out of the annotation
+    flower_copy[bool_mask == False] = 0.0
+    # color mask to separate crypts
+    min_white = (180, 180, 180)
+    max_white = (250, 250, 250)
+    crypt_mask = cv2.inRange(flower_copy, min_white, max_white)
+    result = cv2.bitwise_and(flower_copy, flower_copy, mask=crypt_mask)
+    # split the plot area into 3 columns and take the left pannel
+    plt.subplot(1, 3, 1)
+    plt.axis('off')
+    plt.imshow(flower)  # original image
+    # part 2: middle pannel
+    plt.subplot(1, 3, 2)
+    plt.axis('off')
+    plt.imshow(flower_copy)  # masked image
+    # part 3: right pannel
+    plt.subplot(1, 3, 3)
+    plt.axis('off')
+    plt.imshow(result)  # resulting crypt regions identified
+    plt.savefig(os.path.join(subdirectory, '5.segmentation.jpg'))
+    fraction_crypty = np.sum(result != 0)/np.sum(flower_copy != 0)
+    print('This flower is ~{}% crypt!'.format(int(round(fraction_crypty*100))))
+    return fraction_crypty
 
 
+dir1 = 'C:/Users/st18l084/Dropbox/colon crypt/images266a/masks'
 
+f = one_crypt_extracter(dir1)
 
-
-light_orange = (1, 190, 200)
-dark_orange = (18, 255, 255)
-mask = cv2.inRange(hsv_nemo, light_orange, dark_orange)
-result = cv2.bitwise_and(nemo, nemo, mask)
-plt.subplot(1, 2, 1)
-plt.imshow(mask, cmap="gray")
-plt.subplot(1, 2, 2)
-plt.imshow(result)
-plt.show()
-from matplotlib.colors import hsv_to_rgb
-light_white = (0,0,200)
-dark_white = (145, 60, 255)
-mask_white = cv2.inRange(hsv_nemo, light_white, dark_white)
-results_white = cv2.bitwise_and(nemo, nemo, mask=mask_white)
-plt.subplot(1,2,1)
-plt.imshow(mask_white, cmap='gray')
-plt.subplot(1,2,2)
-plt.imshow(results_white, cmap='gray')
-final_mask = mask+mask_white
-final_result = cv2.bitwise_and(nemo, nemo, mask=final_mask)
-plt.subplot(1,2,1)
-plt.imshow(final_mask, cmap='Purples')
-plt.subplot(1,2,2)
-plt.imshow(final_result)
-plt.show()
-
-blur = cv2.GaussianBlur(final_result, (7,7), 0)
-plt.imshow(blur)
-
-#################################################################################
-# img = cv2.imread(os.path.join(BASE_DIR, WSI))
-img = nemo
-plt.imshow(img)
-Z = img.reshape((-1,3))
-
-# convert to np.float32
-Z = np.float32(Z)
-
-# define criteria, number of clusters(K) and apply kmeans()
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-K = 3
-ret,label,center=cv2.kmeans(Z,K,None,criteria,20,cv2.KMEANS_RANDOM_CENTERS)
-
-# Now convert back into uint8, and make original image
-center = np.uint8(center)
-res = center[label.flatten()]
-res2 = res.reshape((img.shape))
-plt.imshow(label.reshape((img.shape[:2]))==1, cmap='gray')
-cv2.imshow('res2',res2)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-plt.subplot(1,3,1)
-plt.imshow()
-plt.subplot(1,3,2)
-plt.imshow(res2)
-plt.subplot(1,3,3)
-plt.imshow(res2)
-plt.subplot(1,3,4)
-plt.imshow(res2)
