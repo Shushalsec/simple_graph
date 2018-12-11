9a6760422bc639ddf5dbac04da5cfcc495aeb49f
Shushalsec <shtoneyan@gmail.com>
2018-12-11 12:26:59 +0100

    added methods to the class XML

--- Class_graphs.py
+++ Class_graphs.py
@@ -1,7 +1,5 @@
 import os
-from matplotlib.patches import Circle
 import xml.etree.ElementTree as ET
-import shutil
 
 class Attribute:
     """
@@ -26,9 +24,11 @@ class Node:
 
     attr_dict = {}  # dictionary with attribute type as the unique key and the value as the well.. the value of it
 
-    def __init__(self, _id, type, x=0, y=0):
+    def __init__(self, _id, type, save_type = True, x=0, y=0):
         self._id = _id  # node _id
-        self.type = type  # type of the node - central or peripheral
+        # save the node type if needed
+        if save_type:
+            self.attr_dict['type'] = type  # type of the node - central or peripheral
         self.x = x  # node x coordinate of the cell centroid
         self.y = y  # node y coordinate of the cell centroid
 
@@ -68,16 +68,28 @@ class XML(Graph):
         self.root = ET.Element("{}".format(root_tag))
         self.graph = ET.SubElement(self.root, root_child_tag, edgeids="false", edgemode="undirected")
 
-    def one_node_writer(self, save_coords=True, save_node_type=True):
-        pass
-    def XML_writer(self, graph_id):
+    def one_node_writer(self, node_to_add):
+        node = ET.SubElement(self.graph, "node", id="_{}".format(node_to_add._id))
+        for feature in node_to_add.attr_dict.keys():
+            attr_x = ET.SubElement(node, "attr", name=feature)
+            _float = ET.SubElement(attr_x, "float").text = str(node_to_add.attr_dict[feature])
+
+    def one_edge_writer(self, edge_to_add, star_shaped_graph=True):
+        if star_shaped_graph:
+            edge = ET.SubElement(self.graph, "edge", _from="_0", _to="_{}".format(edge_to_add._to))
+        else:
+            edge = ET.SubElement(self.graph, "edge", _from="_{}".format(edge_to_add._from), _to="_{}".format(edge_to_add._to))
+
+    def XML_writer(self, dst_path, graph_id=0):
         self.graph.set(id="graph_{}".format(graph_id))
+        for node in self.nodes:
+            XML.one_node_writer(self, node)
+        tree = ET.ElementTree(self.root)
+        tree.write((os.path.join(dst_path, "{}-graph.xhtml".format(graph_id))))  # for opening in a browser
+        tree.write((os.path.join(dst_path, "graph_{}.gxl".format(graph_id))))  # for using in GED software
 
 
-
-
-
-
-
-
+mean_od = Attribute('mean OD', 10)
+mean_darkness = Attribute('darkness', 2)
+nano = Node(1, 0, 0, 0)
 
